222554c1734bcb1625a439b776d3139c08ec870f
Prateek <prateekkumar4347@gmail.com>
2024-08-13 16:15:59 +0530

    learning creating project through vite

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,11 +3,11 @@
 import Chai from "./Chai"
 
 function App() {
-
+  const username = "chai aur code"
   return (
   <>
    <Chai/>
-   <h1>chai aur react</h1>
+   <h1>chai aur react {username}</h1>
    <p>hi there</p>
   </>
   )
